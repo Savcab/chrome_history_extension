6f084513230be6135b1bd120456c101643e59065
Savcab <felixchenyijun@gmail.com>
2024-11-21 14:00:25 -0800

    updated .gitignore

--- overrides/history/dist/main.js
+++ overrides/history/dist/main.js
@@ -4274,20 +4274,20 @@ const startOfDay = new Date();
 startOfDay.setHours(0, 0, 0, 0);
 const startOfDayTimestamp = startOfDay.getTime();
 console.log("startOfDayTimestamp: " + startOfDayTimestamp);
-// chrome.history.search({
-//     text: '',
-//     startTime: startOfDayTimestamp,
-//     maxResults: 1000000 // basically no upper limit
-// }, function(historyItems) {
-//     console.log("in search")
-//     console.log(historyItems);
-// });
-// // FOR TESTING
-// (async function(){
-//     console.log(chrome);
-//     console.log("currScreentime: ", await chrome.storage.local.get(null));
-//     console.log("lastUserEvent: ", await chrome.storage.local.get("lastUserEvent"));
-// })();
+chrome.history.search({
+    text: '',
+    startTime: startOfDayTimestamp,
+    maxResults: 1000000 // basically no upper limit
+}, function (historyItems) {
+    console.log("in search");
+    console.log(historyItems);
+});
+// FOR TESTING
+(async function () {
+    console.log(chrome);
+    console.log("currScreentime: ", await chrome.storage.local.get(null));
+    console.log("lastUserEvent: ", await chrome.storage.local.get("lastUserEvent"));
+})();
 // Add the single Lit component that is the entire page
 document.querySelector('body').innerHTML = '<lit-page></lit-page>';
 // add styling tags on body
